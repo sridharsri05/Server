cc98db84875c20643e6fbe8a736b268c6d3cff9a
Sridhar <99551606+sridharsri05@users.noreply.github.com>
2024-03-27 21:54:41 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,7 +105,7 @@ const google = async (req, res) => {
 
 const signup = async function (req, res) {
     try {
-        const { username, email, password, role } = req.body;
+        const { username, email, password, role,profilePicture } = req.body;
 
         // Check if the email is already registered
         const existingUser = await User.findOne({ email });
@@ -114,12 +114,13 @@ const signup = async function (req, res) {
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
-
+        const defaultProfilepic ="https://imgs.search.brave.com/Untz9IPAq0oiUcfN8fk7HLba5S-y5zmEOXfDG5xp1zk/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE0/NDQ3MDM2ODY5ODEt/YTNhYmJjNGQ0ZmUz/P3E9ODAmdz0xMDAw/JmF1dG89Zm9ybWF0/JmZpdD1jcm9wJml4/bGliPXJiLTQuMC4z/Jml4aWQ9TTN3eE1q/QTNmREI4TUh4elpX/RnlZMmg4T0h4OGNH/bGpkSFZ5Wlh4bGJu/d3dmSHd3Zkh4OE1B/PT0.jpeg",
         const newUser = new User({
             username,
             email,
             password: hashedPassword,
             role: role || 'user',
+            profilePicture:profilePicture || defaultProfilepic;
         });
 
         await newUser.save();
