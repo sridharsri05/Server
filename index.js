09db8370953e9762d765f65b6cd8cad5ab4bab0e
Sridhar <srissri441@gmail.com>
2024-09-10 13:55:20 +0530

    netlify config

--- index.js
+++ index.js
@@ -1,39 +1,78 @@
-// api/index.js
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require("path")
+// //index.js
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const path = require("path")
 
-const config = require("./config.js");
-const routes = require("./routes/app.js");
+// const config = require("./config.js");
+// const routes = require("./routes/app.js");
+
+// const app = express();
+
+// app.use(express.json());
+// const corsOptions = {
+//     origin: "https://movie-app-ruddy-nine.vercel.app",
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+//     preflightContinue: true,
+//     optionsSuccessStatus: 204
+// };
+
+// app.use(cors(corsOptions));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// // Connect to MongoDB
+// mongoose
+//     .connect(config.MONGODB_URI)
+//     .then(() => {
+//         console.log("MongoDB connected successfully");
+//     })
+//     .catch((error) => {
+//         console.error("MongoDB connection error:", error);
+//     });
+// // const port = 3001;
+// // app.listen(port, () => {
+// //     console.log(`Server is running on port ${port}`);
+// // });
+// // // Use routes
+// app.use("/", routes);
+// module.exports = app;
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const serverless = require('serverless-http');
+
+const config = require('./config.js');
+const routes = require('./routes/app.js');
 
 const app = express();
-
 app.use(express.json());
+
 const corsOptions = {
-    origin: "https://movie-app-ruddy-nine.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    origin: 'https://movie-app-ruddy-nine.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: true,
     optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MongoDB
 mongoose
     .connect(config.MONGODB_URI)
     .then(() => {
-        console.log("MongoDB connected successfully");
+        console.log('MongoDB connected successfully');
     })
     .catch((error) => {
-        console.error("MongoDB connection error:", error);
+        console.error('MongoDB connection error:', error);
     });
-// const port = 3001;
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-// // Use routes
-app.use("/", routes);
-module.exports = app;
 
+// Use routes
+app.use('/', routes);
+
+// Export the serverless function handler
+module.exports.handler = serverless(app);
